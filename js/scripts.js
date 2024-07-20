b075218ab37ea5bd4c1ae15363127d508a269b46
Sam <sambilheimer@gmail.com>
2024-07-20 19:56:37 -0400

    light mode and styles and transitions

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Document is ready!');
 });
+
+document.getElementById('toggle').addEventListener('click', function() {
+    document.getElementById('bodyStyle').classList.toggle('lightmode');
+});
